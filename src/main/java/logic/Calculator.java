c4070124499eab7d7ee3dc3414ce3b2db4d9aa75
4719229 <103704398+4719229@users.noreply.github.com>
2023-05-19 10:00:53 +0900

    Update Calculator.java

--- src/main/java/logic/Calculator.java
+++ src/main/java/logic/Calculator.java
@@ -1,3 +1,19 @@
-public class Calculator {
+package logic;
 
+
+public class Calculator {
+    public static int time(int a, int b){
+
+        return a * b ;
+    }
+
+    public static double divide(int a, int b){
+        if(b==0){
+            return 0.0;
+        }else{
+
+            return (double)a/b;
+
+        }
+    }
 }
